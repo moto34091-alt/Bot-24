6a9a6c26679215d746e1d307dd37585387c48a2a
Support Vimba boss <moto34091@gmail.com>
2026-05-19 08:56:51 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,24 +7,27 @@ app = Flask(__name__)
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_telegram(message):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    data = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-
-    requests.post(url, data=data)
-
 @app.route("/")
 def home():
     return "BOT ONLINE"
 
 @app.route("/signal")
 def signal():
-    send_telegram("📈 BOT CONNECTÉ AVEC SUCCÈS")
+
+    if not TOKEN or not CHAT_ID:
+        return "TOKEN OU CHAT_ID MANQUANT"
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": "📈 BOT CONNECTÉ AVEC SUCCÈS"
+    }
+
+    requests.post(url, data=data)
+
     return "SIGNAL ENVOYÉ"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
